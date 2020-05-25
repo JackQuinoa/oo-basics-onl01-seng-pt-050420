bc6aa80578adde5176bc40334fd0daf8dae5c59b
Jack Quinoa <jquinoa208@gmail.com>
2020-05-25 19:19:30 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,14 @@
+class Shoe
+  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts "The shoe has been repaired"
+    shoe.condition = "new"
+  end
+end
